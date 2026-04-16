c175be75f959bbdfac8ca96a094c1fe128b28a2e
kamleshsony97500-wq <kamleshsony97500@gmail.com>
2026-04-16 22:17:30 +0400

    Update route.ts

--- src/app/api/order/stripe/webhooks/route.ts
+++ src/app/api/order/stripe/webhooks/route.ts
@@ -26,7 +26,7 @@ export async function POST(req: NextRequest) {
     if (event?.type === "checkout.session.completed") {
         const session = event.data.object
         await connectDB()
-        await orderModel.findById(session?.metadata?.orderId, {
+        await orderModel.findByIdAndUpdate(session?.metadata?.orderId, {
             isPaid: true
         });
 
